26849b7095b4e40eb73f82177a9b1366a839c7fd
Carl LeBlond <carl.leblond@iup.edu>
2020-06-06 22:25:46 -0400

    update for app store

--- register.php
+++ register.php
@@ -3,5 +3,32 @@ $REGISTER_LTI2 = array(
 "name" => "JSMol Models",
 "FontAwesome" => "fa-server",
 "short_name" => "JSMol Models",
-"description" => "3D model kit based on JSmol!"
+"description" => "3D model kit based on JSmol!",
+
+ "messages" => array("launch", "launch_grade"),
+    "privacy_level" => "anonymous",  // anonymous, name_only, public
+    "license" => "Apache",
+    "languages" => array(
+        "English"
+    ),
+    "analytics" => array(
+        "internal"
+    ),
+ "placements" => array(
+        /*
+        "course_navigation", "homework_submission",
+        "course_home_submission", "editor_button",
+        "link_selection", "migration_selection", "resource_selection",
+        "tool_configuration", "user_navigation"
+        */
+    ),
+    "screen_shots" => array(
+        "store/screen-01.png",
+        "store/screen-02.png",
+        "store/screen-03.png",
+        "store/screen-views.png",
+        "store/screen-analytics.png"
+    )
+
+
 );
