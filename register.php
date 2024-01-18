0b7d3d1ce1a615351170476a07a1c74976257654
cleblond <cleblond@iup.edu>
2024-01-18 15:46:46 -0500

    updates

--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 $REGISTER_LTI2 = array(
 "name" => "JSMol Models",
 "FontAwesome" => "fa-server",
+"tool_phase" => "hidden",
 "short_name" => "JSMol Models",
 "description" => "3D model kit based on JSmol!",
 
